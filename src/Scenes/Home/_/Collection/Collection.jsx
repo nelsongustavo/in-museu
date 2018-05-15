42001d87022b19c7c3e0f76af9ad646d62196a44
jhonyburil <jhonyburil@gmail.com>
2018-05-14 23:18:46 -0300

    finish styling for collection

--- src/Scenes/Home/_/Collection/Collection.jsx
+++ src/Scenes/Home/_/Collection/Collection.jsx
@@ -10,28 +10,37 @@ export default function Collection() {
       	<Row>
       		<Col md={12} className="header-section">
 						<h1>Acervo</h1>
+            <div className="header-line"></div>
 					</Col>
       	</Row>
       	<Row className="section-gallery">
       		<Col md={3}>
-      			<img src="/images/foto_07.png" alt="" />
-      			<h3>Exposição da Nau Medusa em miniatura</h3>
-      			<time datetime="15/05/2018">15 MAIO 2018</time>
+            <a href="#" title="Exposição da Nau Medusa em miniatura - 15/05/2018">
+        			<img src="/images/foto_07.png" alt="" />
+        			<h3>Exposição da Nau Medusa em miniatura</h3>
+        			<time datetime="15/05/2018">15 MAIO 2018</time>
+            </a>
       		</Col>
       		<Col md={3}>
-      			<img src="/images/foto_02.png" alt="" />
-      			<h3>Caixa Centenária da Independência</h3>
-      			<time datetime="15/05/2018">15 MAIO 2018</time>
+      			<a href="#" title="Caixa Centenária da Independência - 15/05/2018">
+              <img src="/images/foto_02.png" alt="" />
+        			<h3>Caixa Centenária da Independência</h3>
+        			<time datetime="15/05/2018">15 MAIO 2018</time>
+            </a>
       		</Col>
       		<Col md={3}>
-      			<img src="/images/foto_27.png" alt="" />
-      			<h3>Teclado Perfurador Monotipo Joana França</h3>
-      			<time datetime="15/05/2018">15 MAIO 2018</time>
+      			<a href="#" title="">
+              <img src="/images/foto_27.png" alt="Teclado Perfurador Monotipo Joana França - 15/05/2018" />
+        			<h3>Teclado Perfurador Monotipo Joana França</h3>
+        			<time datetime="15/05/2018">15 MAIO 2018</time>
+            </a>
       		</Col>
       		<Col md={3}>
-      			<img src="/images/foto_12.png" alt="" />
-      			<h3>Composição Vitrine Florões</h3>
-      			<time datetime="15/05/2018">15 MAIO 2018</time>
+      			<a href="#" title="">
+              <img src="/images/foto_12.png" alt="Composição Vitrine Florões - 15/05/2018" />
+        			<h3>Composição Vitrine Florões</h3>
+        			<time datetime="15/05/2018">15 MAIO 2018</time>
+            </a>
       		</Col>
       	</Row>
       </Grid>
